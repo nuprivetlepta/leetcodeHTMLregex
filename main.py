51fcd17fec7e2e834f9cfe8a20a5051e7d7b3fe7
nuprivetepta <stopkran510@gmail.com>
2022-11-27 15:01:22 +0300

    Создан класс с двумя методами замены символов, так же один тест на сравнение результатов.

---
+++ main.py
@@ -0,0 +1,51 @@
+# URL: https://leetcode.com/problems/html-entity-parser/
+# Задание: на ввод подаётся код HTML, на выходе нужно получить читабельную строку с заменой символов
+# на их обычное представление.
+# Суть проблемы: два идентичных решения, одно в котором идёт распаковка списка кортежей,
+# во втором- пар "ключ" - "значение" из словаря. Метод через список работает, через словарь не работает.
+# При построчном выводе элементов кортежей и ключей словаря
+# и дальнейшем сравнении получаем True- элементы идентичны.
+
+
+import re
+
+
+class Solution(object):
+    def entityparser(self, text):
+        """
+        :type text: str
+        :rtype: str
+        """
+        di = {'&quot;': '\"', '&apos;': '\'', '&amp;': '&', '&gt;': '>', '&lt;': '<', '&frasl;': '/'}
+
+        for key, value in di.items():
+            re.sub(key, value, text)
+
+        return text
+
+    def entitytuple(self, text):
+        entities = [('&quot;', '\"'), ('&apos;', '\''), ('&gt;', '>'), ('&lt;', '<'), ('&frasl;', '/'), ('&amp;', '&')]
+
+        for pat, repl in entities:
+            text = re.sub(pat, repl, text)
+
+        return text
+
+
+class TestCases(object):
+    def test_two_funcs(self, txt: str):
+        with_list = Solution().entitytuple(text=txt)
+        with_dict = Solution().entityparser(text=txt)
+        assert with_dict == with_list
+
+
+if __name__ == '__main__':
+    stri = "&amp; is an HTML entity but &ambassador; is not."
+    method_with_dict = Solution().entityparser(text=stri)
+    method_with_list = Solution().entitytuple(text=stri)
+    print(method_with_dict)
+    print(method_with_list)
+
+    our_test = TestCases().test_two_funcs(stri)
+
+    print('thanx for help')
